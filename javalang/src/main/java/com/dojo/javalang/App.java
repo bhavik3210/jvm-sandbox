d7356ba01f6885ae76fd5c115a06cb3fa4ff283f
bhavik <bpatel5493@gmail.com>
2023-04-17 07:47:10 -0500

    new Java Project

---
+++ javalang/src/main/java/com/dojo/javalang/App.java
@@ -0,0 +1,16 @@
+package com.dojo.javalang;
+
+import com.yoctopuce.YoctoAPI.*;
+
+/**
+ * Simple example of Yoctopuce library usage
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World");
+        System.out.println();
+    }
+}
