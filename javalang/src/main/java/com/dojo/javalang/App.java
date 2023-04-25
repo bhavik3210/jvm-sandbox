1a59f706953752fc7185da4dbda8ca0ef45c9daa
bhavik <bpatel5493@gmail.com>
2023-04-25 07:17:29 -0500

    wip

---
+++ javalang/src/main/java/com/dojo/javalang/App.java
@@ -0,0 +1,15 @@
+package com.dojo.javalang;
+
+
+/**
+ * Simple example of Yoctopuce library usage
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World");
+        System.out.println();
+    }
+}
---
+++ javalang/src/main/java/com/dojo/javalang/model/Product.java
@@ -0,0 +1,9 @@
+package com.dojo.javalang.model;
+
+public record Product(String name, int weight) {
+
+  @Override
+  public String toString() {
+    return "String.format()";
+  }
+}
