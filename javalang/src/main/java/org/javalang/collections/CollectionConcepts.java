0e728c21fb6e21f2699c1948d3b35c2b286d8c17
bhavik <bpatel5493@gmail.com>
2023-05-02 10:02:33 -0500

    wip

--- javalang/src/main/java/org/javalang/collections/CollectionConcepts.java
+++ javalang/src/main/java/org/javalang/collections/CollectionConcepts.java
@@ -5,27 +5,45 @@ import org.javalang.model.Product;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
-public class CollectionConcepts
-{
-    public static void main(String[] args)
-    {
+public class CollectionConcepts {
+    public static void main(String[] args) {
         var door = new Product("Wooden Door", 35);
         var floorPanel = new Product("Floor Panel", 25);
         var window = new Product("Glass Window", 10);
 
-        Collection<Product> products = new ArrayList<>();
-        products.add(door);
-        products.add(floorPanel);
-        products.add(window);
+        // Remove products using basic for each loop (which can replaced with removeIf)
+        Collection<Product> products = getProducts(door, floorPanel, window);
+        products.removeIf(product -> product.weight() > 20);
+        System.out.println("For Each " + products);
 
-       // need to go over each type that was cover in Collection Behaviors Demo
+        // Remove products using Iterator
+        Collection<Product> products2 = getProducts(door, floorPanel, window);
+        Iterator<Product> it = products2.iterator();
+        while (it.hasNext()) {
+            var product = it.next();
+            if (product.weight() > 20) {
+                it.remove();
+            }
+        }
+        System.out.println("Iterator " + products2);
 
+        // Remove using array compare
+        Collection<Product> products3 = getProducts(door, floorPanel, window);
         var toRemove = new ArrayList<Product>();
         toRemove.add(door);
         toRemove.add(floorPanel);
 
-        products.removeAll(toRemove);
-        System.out.println(products);
+        products3.removeAll(toRemove);
+        System.out.println("Array Compare " +products3);
+    }
+
+    public static Collection<Product> getProducts(Product door, Product floorPanel, Product window) {
+        Collection<Product> products = new ArrayList<>();
+        products.add(door);
+        products.add(floorPanel);
+        products.add(window);
+        return products;
     }
 }
