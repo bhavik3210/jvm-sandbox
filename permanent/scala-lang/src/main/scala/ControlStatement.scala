4b9ec69c290574b6721783287ce9ee2d62fad884
bhavik <bhavik@work>
2023-06-30 18:33:59 -0500

    wip

---
+++ permanent/scala-lang/src/main/scala/ControlStatement.scala
@@ -0,0 +1,24 @@
+package dojo.lang.scala
+
+object ControlStatement {
+  def demo(): Unit = {
+    val number = 10
+
+    // Statement
+    if (number > 10) {
+      println("> 10")
+    } else {
+      println("< 10")
+    }
+
+    // Expression
+    val result = if (number > 10) {
+      "> 10"
+    } else if (number == 10) {
+      "== 10"
+    } else {
+      "< 10"
+    }
+    println(result)
+  }
+}
---
+++ permanent/scala-lang/src/main/scala/Loop.scala
@@ -0,0 +1,51 @@
+package dojo.lang.scala
+
+object Loop {
+  def demo(): Unit = {
+    // Generator Expression: Array(1, 2))
+    // Generator: value <- Array(1, 2)
+    val amounts = Array(10, 23, 98, 54, 34)
+    for (value <- amounts) {
+      println(value)
+    }
+
+    //Expression for for loop iteration
+    val decimalAmount: Array[Double] = for (value <- amounts) yield {
+      value.toDouble // You can transform a type
+    }
+    println()
+    for (v <- decimalAmount) {
+      println(v)
+    }
+
+    //Expression with for loop that you can use to transform with conditions
+    val currencies = Array("USD", "MXN", "INR", "NZD", "USD")
+    val amountWithCurrency: Array[String] = for {
+      amount <- decimalAmount
+      currency <- currencies
+      if amount > 20 && (currency == "NZD" || currency == "USD") //without this condition it will return all permutations
+    } yield currency + " " + amount
+    println()
+
+    // For each is a statement
+    amountWithCurrency.foreach(value => print(value + " | "))
+
+
+    // While loop
+    println()
+    var i = 1
+    while (i <= 10) {
+      println(i)
+      i += 1
+    }
+
+    // do while loop
+    println()
+    var j = -10
+    do {
+      println(j)
+      j -= 1
+    } while (j > 0)
+  }
+
+}
